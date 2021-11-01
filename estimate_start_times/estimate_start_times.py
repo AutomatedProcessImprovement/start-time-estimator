46237e3357b8e938c6918adaca172bb479fac9a6
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2021-11-01 11:46:36 +0200

    Implement fix methods for non-estimated start timestamps (events with not enablement and no resource availability)  - Instant: make the event instant by setting as start time its own end time.  - Re-estimate: re-estimate the event duration by taking the mode of the estimated processing times of the same activity, if there are no other estimated processing times (i.e. all its events are non-estimated), set instant.

--- estimate_start_times/estimate_start_times.py
+++ estimate_start_times/estimate_start_times.py
@@ -1,17 +1,22 @@
 #
 # Author: David Chapela de la Campa
 #
+import enum
 from pm4py.objects.log.obj import EventLog
+from statistics import mode
 
 
-def estimate_start_timestamps(event_log, concurrency_oracle, resource_availability) -> EventLog:
+class FixMethod(enum.Enum):
+    SET_INSTANT = 1
+    RE_ESTIMATE = 2
+
+
+def estimate_start_timestamps(event_log,
+                              concurrency_oracle,
+                              resource_availability,
+                              fix_method=FixMethod.SET_INSTANT) -> EventLog:
     # Assign start timestamps
-    i = 0
-    print("Starting inference")
     for trace in event_log:
-        i += 1
-        if i % 100 == 0:
-            print(i)
         for event in trace:
             enabled_time = concurrency_oracle.enabled_since(trace, event)
             available_time = resource_availability.available_since(event['org:resource'], event['time:timestamp'])
@@ -20,4 +25,54 @@ def estimate_start_timestamps(event_log, concurrency_oracle, resource_availabili
                 available_time
             )
     # Fix start times for those events being the first one of the trace and the resource (with initial_time)
+    if fix_method == FixMethod.SET_INSTANT:
+        estimated_event_log = set_instant_non_estimated_start_times(event_log, concurrency_oracle.initial_time)
+    elif fix_method == FixMethod.RE_ESTIMATE:
+        estimated_event_log = fix_non_estimated_start_times(event_log, concurrency_oracle.initial_time)
+    else:
+        print("Unselected fix method for events with no estimated start time! Setting them as instant by default.")
+        estimated_event_log = set_instant_non_estimated_start_times(event_log, concurrency_oracle.initial_time)
+    # Return modified event log
+    return estimated_event_log
+
+
+def set_instant_non_estimated_start_times(event_log, non_estimated_time) -> EventLog:
+    # Identify events with non_estimated as start time
+    # and set their processing time to instant
+    for trace in event_log:
+        for event in trace:
+            if event['start:timestamp'] == non_estimated_time:
+                # Non-estimated, save event to estimate based on statistics
+                event['start:timestamp'] = event['time:timestamp']
+    # Return modified event log
+    return event_log
+
+
+def fix_non_estimated_start_times(event_log, non_estimated_time) -> EventLog:
+    # Identify events with non_estimated as start time
+    # and store the durations of the estimated ones
+    non_estimated_events = []
+    activity_times = {}
+    for trace in event_log:
+        for event in trace:
+            if event['start:timestamp'] == non_estimated_time:
+                # Non-estimated, save event to estimate based on statistics
+                non_estimated_events += [event]
+            else:
+                # Estimated, store estimated time to calculate statistics
+                activity = event['concept:name']
+                processing_time = event['time:timestamp'] - event['start:timestamp']
+                if activity not in activity_times:
+                    activity_times[activity] = [processing_time]
+                else:
+                    activity_times[activity] += [processing_time]
+    # Set as start time the end time - the mode of the processing times (most frequent processing time)
+    for event in non_estimated_events:
+        activity = event['concept:name']
+        if activity in activity_times:
+            event['start:timestamp'] = event['time:timestamp'] - mode(activity_times[activity])
+        else:
+            # If this activity has no estimated times set as instant activity
+            event['start:timestamp'] = event['time:timestamp']
+    # Return modified event log
     return event_log
--- estimate_start_times/test/estimate_start_times_test.py
+++ estimate_start_times/test/estimate_start_times_test.py
@@ -2,19 +2,20 @@ import pm4py
 import pytz
 from estimate_start_times.concurrency_oracle import NoConcurrencyOracle
 from estimate_start_times.estimate_start_times import estimate_start_timestamps
+from estimate_start_times.estimate_start_times import FixMethod
 from estimate_start_times.resource_availability import ResourceAvailability
 from datetime import datetime
 
 
-def test_estimate_start_times():
+def test_estimate_start_times_instant():
     initial_time = datetime.min.replace(tzinfo=pytz.UTC)
     event_log = pm4py.read_xes('.\\event_logs\\test_event_log_1.xes')
     concurrency_oracle = NoConcurrencyOracle(event_log, initial_time)
     resource_availability = ResourceAvailability(event_log, initial_time, 'missing_resource')
     extended_event_log = estimate_start_timestamps(event_log, concurrency_oracle, resource_availability)
-    # The start time of initial events is the initial_time
-    assert extended_event_log[0][0]['start:timestamp'] == initial_time
-    assert extended_event_log[3][0]['start:timestamp'] == initial_time
+    # The start time of initial events is the end time (instant events)
+    assert extended_event_log[0][0]['start:timestamp'] == extended_event_log[0][0]['time:timestamp']
+    assert extended_event_log[3][0]['start:timestamp'] == extended_event_log[3][0]['time:timestamp']
     # The start time of an event with its resource free but immediately
     # following its previous one is the end time of the previous one.
     assert extended_event_log[1][3]['start:timestamp'] == event_log[1][2]['time:timestamp']
@@ -23,3 +24,17 @@ def test_estimate_start_times():
     # busy in other activities is the end time of its resource's last activity.
     assert extended_event_log[3][3]['start:timestamp'] == event_log[2][2]['time:timestamp']
     assert extended_event_log[3][4]['start:timestamp'] == event_log[1][4]['time:timestamp']
+
+
+def test_estimate_start_times_re_estimate():
+    initial_time = datetime.min.replace(tzinfo=pytz.UTC)
+    event_log = pm4py.read_xes('.\\event_logs\\test_event_log_1.xes')
+    concurrency_oracle = NoConcurrencyOracle(event_log, initial_time)
+    resource_availability = ResourceAvailability(event_log, initial_time, 'missing_resource')
+    extended_event_log = estimate_start_timestamps(event_log, concurrency_oracle,
+                                                   resource_availability, FixMethod.RE_ESTIMATE)
+    # The start time of initial events is the most frequent processing time
+    assert extended_event_log[0][0]['start:timestamp'] == extended_event_log[0][0]['time:timestamp'] - \
+           (extended_event_log[2][0]['time:timestamp'] - extended_event_log[0][0]['time:timestamp'])
+    assert extended_event_log[1][0]['start:timestamp'] == extended_event_log[1][0]['time:timestamp'] - \
+           (extended_event_log[2][0]['time:timestamp'] - extended_event_log[0][0]['time:timestamp'])
