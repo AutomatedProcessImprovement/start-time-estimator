8a25d89be0aacb263bd93fc7728c5cafe945afb7
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2021-11-19 20:24:39 +0200

    Document Configuration class

--- estimate_start_times/config.py
+++ estimate_start_times/config.py
@@ -57,6 +57,26 @@ class HeuristicsThresholds:
 
 @dataclass
 class Configuration:
+    """Class storing the configuration parameters for the start time estimation.
+
+    Attributes:
+        log_ids                     Identifiers for each key element (e.g. executed activity or resource).
+        concurrency_oracle_type     Concurrency oracle to use (e.g. heuristics miner's concurrency oracle).
+        resource_availability_type  Resource availability engine to use (e.g. using resource calendars).
+        missing_resource            String to identify the events with missing resource (it is avoided in the resource availability
+                                    calculation).
+        non_estimated_time          Time to use as value when the start time cannot be estimated (later re-estimated with
+                                    [re_estimation_method].
+        re_estimation_method        Method (e.g. median) to re-estimate the start times that couldn't be estimated due to lack of resource
+                                    availability and causal predecessors.
+        bot_resources               Set of resource IDs corresponding bots, in order to set their events as instant.
+        instant_activities          Set of instantaneous activities, in order to set their events as instant.
+        heuristics_thresholds       Thresholds for the heuristics concurrency oracle (only used is this oracle is selected as
+                                    [concurrency_oracle_type].
+        outlier_statistic           Statistic (e.g. median) to calculate from the distribution of each activity durations to consider and
+                                    re-estimate the outlier events which estimated duration is higher.
+        outlier_threshold           Threshold to control outliers, those events with estimated durations over
+    """
     log_ids: EventLogIDs = DEFAULT_CSV_IDS
     concurrency_oracle_type: ConcurrencyOracleType = ConcurrencyOracleType.ALPHA
     resource_availability_type: ResourceAvailabilityType = ResourceAvailabilityType.SIMPLE
