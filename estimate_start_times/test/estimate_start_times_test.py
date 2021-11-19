5420ed715ef06f01f8788853681342362df5038d
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2021-11-19 20:24:18 +0200

    Implement tests for threshold option to control outliers

--- estimate_start_times/test/estimate_start_times_test.py
+++ estimate_start_times/test/estimate_start_times_test.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 import pandas as pd
 
-from config import ConcurrencyOracleType, Configuration, DEFAULT_XES_IDS, ReEstimationMethod, ResourceAvailabilityType
+from config import ConcurrencyOracleType, Configuration, DEFAULT_XES_IDS, ReEstimationMethod, ResourceAvailabilityType, OutlierStatistic
 from estimate_start_times.estimate_start_times import StartTimeEstimator
 from event_log_readers import read_csv_log, read_xes_log
 
@@ -133,6 +133,67 @@ def test_bot_resources_and_instant_activities_el():
     # busy in other activities is the end time of its resource's last activity.
     assert extended_event_log[3][3][config.log_ids.start_timestamp] == event_log[2][2][config.log_ids.end_timestamp]
     assert extended_event_log[3][4][config.log_ids.start_timestamp] == event_log[1][4][config.log_ids.end_timestamp]
+
+
+def test_repair_activities_with_duration_over_threshold_df():
+    config = Configuration(
+        re_estimation_method=ReEstimationMethod.MEDIAN,
+        concurrency_oracle_type=ConcurrencyOracleType.NONE,
+        resource_availability_type=ResourceAvailabilityType.SIMPLE,
+        outlier_statistic=OutlierStatistic.MEDIAN,
+        outlier_threshold=1.6
+    )
+    event_log = read_csv_log('./assets/test_event_log_1.csv', config)
+    # Estimate start times
+    start_time_estimator = StartTimeEstimator(event_log, config)
+    extended_event_log = start_time_estimator.estimate()
+    # The start time of an event (with duration under the threshold) with its resource
+    # free but immediately following its previous one is the end time of the previous one.
+    second_trace = extended_event_log[extended_event_log[config.log_ids.case] == 'trace-02']
+    assert second_trace.iloc[3][config.log_ids.start_timestamp] == second_trace.iloc[2][config.log_ids.end_timestamp]
+    # The start time of an event (with duration under the threshold) enabled for a long time
+    # but with its resource busy in other activities is the end time of its resource's last activity.
+    third_trace = extended_event_log[extended_event_log[config.log_ids.case] == 'trace-03']
+    fourth_trace = extended_event_log[extended_event_log[config.log_ids.case] == 'trace-04']
+    assert fourth_trace.iloc[3][config.log_ids.start_timestamp] == third_trace.iloc[2][config.log_ids.end_timestamp]
+    assert fourth_trace.iloc[4][config.log_ids.start_timestamp] == second_trace.iloc[4][config.log_ids.end_timestamp]
+    # The events with estimated durations over the threshold where re-estimated
+    first_trace = extended_event_log[extended_event_log[config.log_ids.case] == 'trace-01']
+    assert first_trace.iloc[1][config.log_ids.start_timestamp] == \
+           first_trace.iloc[1][config.log_ids.end_timestamp] - timedelta(minutes=49.6)
+    assert third_trace.iloc[2][config.log_ids.start_timestamp] == \
+           third_trace.iloc[2][config.log_ids.end_timestamp] - timedelta(minutes=11.2)
+    assert first_trace.iloc[6][config.log_ids.start_timestamp] == \
+           first_trace.iloc[6][config.log_ids.end_timestamp] - timedelta(minutes=38.4)
+
+
+def test_repair_activities_with_duration_over_threshold_el():
+    config = Configuration(
+        log_ids=DEFAULT_XES_IDS,
+        re_estimation_method=ReEstimationMethod.MEDIAN,
+        concurrency_oracle_type=ConcurrencyOracleType.NONE,
+        resource_availability_type=ResourceAvailabilityType.SIMPLE,
+        outlier_statistic=OutlierStatistic.MEDIAN,
+        outlier_threshold=1.6
+    )
+    event_log = read_xes_log('./assets/test_event_log_1.xes', config)
+    # Estimate start times
+    start_time_estimator = StartTimeEstimator(event_log, config)
+    extended_event_log = start_time_estimator.estimate()
+    # The start time of an event (with duration under the threshold) with its resource
+    # free but immediately following its previous one is the end time of the previous one.
+    assert extended_event_log[1][3][config.log_ids.start_timestamp] == extended_event_log[1][2][config.log_ids.end_timestamp]
+    # The start time of an event (with duration under the threshold) enabled for a long time
+    # but with its resource busy in other activities is the end time of its resource's last activity.
+    assert extended_event_log[3][3][config.log_ids.start_timestamp] == extended_event_log[2][2][config.log_ids.end_timestamp]
+    assert extended_event_log[3][4][config.log_ids.start_timestamp] == extended_event_log[1][4][config.log_ids.end_timestamp]
+    # The events with estimated durations over the threshold where re-estimated
+    assert extended_event_log[0][1][config.log_ids.start_timestamp] == \
+           extended_event_log[0][1][config.log_ids.end_timestamp] - timedelta(minutes=49.6)
+    assert extended_event_log[2][2][config.log_ids.start_timestamp] == \
+           extended_event_log[2][2][config.log_ids.end_timestamp] - timedelta(minutes=11.2)
+    assert extended_event_log[0][6][config.log_ids.start_timestamp] == \
+           extended_event_log[0][6][config.log_ids.end_timestamp] - timedelta(minutes=38.4)
 
 
 def test_estimate_start_times_mode_df():
