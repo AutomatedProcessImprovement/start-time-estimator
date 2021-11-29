ebfcd865c04a46e436983d4d6edd0b2385f23ece
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2021-11-29 19:04:51 +0200

    Refactor experimentation pipeline

--- estimate_start_times/main.py
+++ estimate_start_times/main.py
@@ -1,18 +1,13 @@
-import os
 import time
-
-import pandas as pd
-from pm4py.objects.conversion.log import converter as log_converter
-from pm4py.objects.log.exporter.xes import exporter as xes_exporter
-from pm4py.objects.log.obj import EventLog
 
 from config import Configuration, DEFAULT_XES_IDS, ReEstimationMethod, ConcurrencyOracleType, ResourceAvailabilityType, \
     HeuristicsThresholds, OutlierStatistic
 from estimate_start_times import StartTimeEstimator
 from event_log_readers import read_event_log
+from event_log_writers import write_event_log
 
 
-def run_estimation(event_log_path, configuration, extension):
+def run_estimation(event_log_path, configuration, output_log_path):
     print("\nProcessing event log {}".format(event_log_path))
     # Read event log
     event_log = read_event_log(event_log_path, configuration)
@@ -25,100 +20,31 @@ def run_estimation(event_log_path, configuration, extension):
     extended_event_log = start_time_estimator.estimate()
     end_time = time.process_time()
     print("Estimation finished ({}s).".format(end_time - start_time))
-
-    if type(extended_event_log) is EventLog:
-        # Translate to pd.DataFrame
-        extended_event_log = log_converter \
-            .apply(extended_event_log, variant=log_converter.Variants.TO_DATA_FRAME) \
-            .rename(
-                columns={
-                    'case:{}'.format(configuration.log_ids.case): 'case:concept:name',
-                    configuration.log_ids.activity: 'concept:name',
-                    configuration.log_ids.start_timestamp: 'time:start',
-                    configuration.log_ids.end_timestamp: 'time:timestamp',
-                    configuration.log_ids.resource: 'org:resource'
-                }
-            )
-    elif type(extended_event_log) is pd.DataFrame:
-        # Rename the columns to fit SIMOD format
-        extended_event_log = extended_event_log.rename(
-            columns={
-                configuration.log_ids.case: 'case:concept:name',
-                configuration.log_ids.activity: 'concept:name',
-                configuration.log_ids.start_timestamp: 'time:start',
-                configuration.log_ids.end_timestamp: 'time:timestamp',
-                configuration.log_ids.resource: 'org:resource'
-            }
-        )
-
-    # Transform to SIMOD input format: two events with lifecycles
-    extended_event_log = extended_event_log[
-        ['case:concept:name', 'concept:name', 'time:start', 'time:timestamp', 'org:resource']
-    ].sort_values(by=['case:concept:name', 'time:timestamp'])
-    extended_event_log_start = extended_event_log.copy()
-    extended_event_log_start['lifecycle:transition'] = 'start'
-    extended_event_log_start['time:timestamp'] = extended_event_log_start['time:start']
-    extended_event_log_end = extended_event_log.copy()
-    extended_event_log_end['lifecycle:transition'] = 'complete'
-    extended_event_log = pd.concat([extended_event_log_start, extended_event_log_end]) \
-        .drop(['time:start'], axis=1) \
-        .rename_axis('index') \
-        .sort_values(by=['time:timestamp', 'index', 'lifecycle:transition'], ascending=[True, True, False])
-    # Unify date format to 'yyyy-mm-ddThh:mm:ss.sss+zz:zz' (chapuza)
-    extended_event_log['time:timestamp'] = \
-        extended_event_log['time:timestamp'].apply(lambda x: x.strftime('%Y-%m-%dT%H:%M:%S.%f')).apply(
-            lambda x: x[:-3]) + \
-        extended_event_log['time:timestamp'].apply(lambda x: x.strftime("%z")).apply(lambda x: x[:-2]) + \
-        ":" + \
-        extended_event_log['time:timestamp'].apply(lambda x: x.strftime("%z")).apply(lambda x: x[-2:])
-    # Transform to event log
-    extended_event_log = log_converter.apply(extended_event_log, variant=log_converter.Variants.TO_EVENT_LOG)
-    # Export event log
-    output_path = event_log_path.replace(extension, "_estimated_str.xes")
-    xes_exporter.apply(extended_event_log, output_path)
-    # Change 'time:timestamp' from 'string' to 'date' (chapuza)
-    change_string_by_date(output_path)
-
-
-def change_string_by_date(file_path):
-    with open(file_path, 'r') as input_file, open(file_path.replace("_str.xes", ".xes"), 'w') as output_file:
-        for line in input_file.readlines():
-            if "<string key=\"time:timestamp\"" in line:
-                output_file.write(line.replace("<string key=\"time:timestamp\"", "<date key=\"time:timestamp\""))
-            else:
-                output_file.write(line)
-    os.remove(file_path)
+    # Export
+    write_event_log(extended_event_log, output_log_path)
 
 
 def main():
-    # Consulta Data Mining 2016 - 2018
-    config = Configuration(
-        log_ids=DEFAULT_XES_IDS,
-        re_estimation_method=ReEstimationMethod.MEDIAN,
-        concurrency_oracle_type=ConcurrencyOracleType.HEURISTICS,
-        resource_availability_type=ResourceAvailabilityType.SIMPLE,
-        bot_resources={"Start", "End"},
-        # instant_activities={"Notificacion estudiante cancelacion soli", "Traer informacion estudiante - banner", "Start", "End"},
-        instant_activities={"Traer informacion estudiante - banner", "Start", "End"},
-        heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
-        outlier_statistic=OutlierStatistic.MEDIAN,
-        outlier_threshold=2.0
-    )
-    run_estimation("../event_logs/ConsultaDataMining201618.xes.gz", config, ".xes.gz")
+    # General options
+    outlier_threshold = 2.0
+    folder = "heur-median-2"
 
-    # Production
+    # ------------------------------------ #
+    # ---------- SYNTHETIC LOGS ---------- #
+    # ------------------------------------ #
+
+    # Confidential
     config = Configuration(
         log_ids=DEFAULT_XES_IDS,
         re_estimation_method=ReEstimationMethod.MEDIAN,
         concurrency_oracle_type=ConcurrencyOracleType.HEURISTICS,
         resource_availability_type=ResourceAvailabilityType.SIMPLE,
-        bot_resources={"Start", "End"},
-        instant_activities={"Start", "End"},
         heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
         outlier_statistic=OutlierStatistic.MEDIAN,
-        outlier_threshold=2.0
+        outlier_threshold=outlier_threshold
     )
-    run_estimation("../event_logs/Production.xes.gz", config, ".xes.gz")
+    run_estimation("../event_logs/confidential.xes.gz", config,
+                   "../event_logs/{}/confidential_estimated.xes.gz".format(folder))
 
     # CVS Pharmacy
     config = Configuration(
@@ -127,14 +53,28 @@ def main():
         concurrency_oracle_type=ConcurrencyOracleType.HEURISTICS,
         resource_availability_type=ResourceAvailabilityType.SIMPLE,
         bot_resources={"Pharmacy System-000001"},
-        instant_activities={"Prescription fulfilled", "Prescription received", "EVENT 19 END ERROR"},
         heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
         outlier_statistic=OutlierStatistic.MEDIAN,
-        outlier_threshold=2.0
+        outlier_threshold=outlier_threshold
     )
-    run_estimation("../event_logs/cvs_pharmacy.xes.gz", config, ".xes.gz")
+    run_estimation("../event_logs/cvs_pharmacy.xes.gz", config,
+                   "../event_logs/{}/cvs_pharmacy_estimated.xes.gz".format(folder))
 
-    # POC Process Mining
+    # Loan Application
+    config = Configuration(
+        log_ids=DEFAULT_XES_IDS,
+        re_estimation_method=ReEstimationMethod.MEDIAN,
+        concurrency_oracle_type=ConcurrencyOracleType.HEURISTICS,
+        resource_availability_type=ResourceAvailabilityType.SIMPLE,
+        heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
+        instant_activities={"loan application returned"},
+        outlier_statistic=OutlierStatistic.MEDIAN,
+        outlier_threshold=outlier_threshold
+    )
+    run_estimation("../event_logs/Loan_Application.xes.gz", config,
+                   "../event_logs/{}/Loan_Application_estimated.xes.gz".format(folder))
+
+    # Procure to Pay
     config = Configuration(
         log_ids=DEFAULT_XES_IDS,
         re_estimation_method=ReEstimationMethod.MEDIAN,
@@ -142,11 +82,16 @@ def main():
         resource_availability_type=ResourceAvailabilityType.SIMPLE,
         heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
         outlier_statistic=OutlierStatistic.MEDIAN,
-        outlier_threshold=2.0
+        outlier_threshold=outlier_threshold
     )
-    run_estimation("../event_logs/poc_processmining.xes.gz", config, ".xes.gz")
+    run_estimation("../event_logs/Procure_to_Pay.xes.gz", config,
+                   "../event_logs/{}/Procure_to_Pay_estimated.xes.gz".format(folder))
 
-    # BPIC 2017
+    # ------------------------------- #
+    # ---------- REAL LOGS ---------- #
+    # ------------------------------- #
+
+    # Application to Approval Government Agency
     config = Configuration(
         log_ids=DEFAULT_XES_IDS,
         re_estimation_method=ReEstimationMethod.MEDIAN,
@@ -154,61 +99,10 @@ def main():
         resource_availability_type=ResourceAvailabilityType.SIMPLE,
         heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
         outlier_statistic=OutlierStatistic.MEDIAN,
-        outlier_threshold=2.0
+        outlier_threshold=outlier_threshold
     )
-    run_estimation("../event_logs/BPI_Challenge_2017_W_Two_TS.xes.gz", config, ".xes.gz")
-
-    # Insurance
-    config = Configuration(
-        log_ids=DEFAULT_XES_IDS,
-        re_estimation_method=ReEstimationMethod.MEDIAN,
-        concurrency_oracle_type=ConcurrencyOracleType.HEURISTICS,
-        resource_availability_type=ResourceAvailabilityType.SIMPLE,
-        heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
-        outlier_statistic=OutlierStatistic.MEDIAN,
-        outlier_threshold=2.0
-    )
-    run_estimation("../event_logs/insurance.xes.gz", config, ".xes.gz")
-
-    # Confidential 1000
-    config = Configuration(
-        log_ids=DEFAULT_XES_IDS,
-        re_estimation_method=ReEstimationMethod.MEDIAN,
-        concurrency_oracle_type=ConcurrencyOracleType.HEURISTICS,
-        resource_availability_type=ResourceAvailabilityType.SIMPLE,
-        instant_activities={"start", "end", "END1"},
-        heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
-        outlier_statistic=OutlierStatistic.MEDIAN,
-        outlier_threshold=2.0
-    )
-    run_estimation("../event_logs/confidential_1000.xes.gz", config, ".xes.gz")
-
-    # Confidential 2000
-    config = Configuration(
-        log_ids=DEFAULT_XES_IDS,
-        re_estimation_method=ReEstimationMethod.MEDIAN,
-        concurrency_oracle_type=ConcurrencyOracleType.HEURISTICS,
-        resource_availability_type=ResourceAvailabilityType.SIMPLE,
-        instant_activities={"start", "end", "END1"},
-        heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
-        outlier_statistic=OutlierStatistic.MEDIAN,
-        outlier_threshold=2.0
-    )
-    run_estimation("../event_logs/confidential_2000.xes.gz", config, ".xes.gz")
-
-    # Purchasing Example
-    config = Configuration(
-        log_ids=DEFAULT_XES_IDS,
-        re_estimation_method=ReEstimationMethod.MEDIAN,
-        concurrency_oracle_type=ConcurrencyOracleType.HEURISTICS,
-        resource_availability_type=ResourceAvailabilityType.SIMPLE,
-        # instant_activities={"Choose best option", "Send Invoice", "Authorize Supplier's Invoice payment"},
-        instant_activities={"Choose best option"},
-        heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
-        outlier_statistic=OutlierStatistic.MEDIAN,
-        outlier_threshold=2.0
-    )
-    run_estimation("../event_logs/PurchasingExample.xes.gz", config, ".xes.gz")
+    run_estimation("../event_logs/Application_to_Approval_Government_Agency.xes.gz", config,
+                   "../event_logs/{}/Application_to_Approval_Government_Agency_estimated.xes.gz".format(folder))
 
     # BPIC 2012
     config = Configuration(
@@ -218,9 +112,23 @@ def main():
         resource_availability_type=ResourceAvailabilityType.SIMPLE,
         heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
         outlier_statistic=OutlierStatistic.MEDIAN,
-        outlier_threshold=2.0
+        outlier_threshold=outlier_threshold
     )
-    run_estimation("../event_logs/BPI_Challenge_2012_W_Two_TS.xes.gz", config, ".xes.gz")
+    run_estimation("../event_logs/BPI_Challenge_2012_W_Two_TS.xes.gz", config,
+                   "../event_logs/{}/BPI_Challenge_2012_W_Two_TS_estimated.xes.gz".format(folder))
+
+    # BPIC 2017
+    config = Configuration(
+        log_ids=DEFAULT_XES_IDS,
+        re_estimation_method=ReEstimationMethod.MEDIAN,
+        concurrency_oracle_type=ConcurrencyOracleType.HEURISTICS,
+        resource_availability_type=ResourceAvailabilityType.SIMPLE,
+        heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
+        outlier_statistic=OutlierStatistic.MEDIAN,
+        outlier_threshold=outlier_threshold
+    )
+    run_estimation("../event_logs/BPI_Challenge_2017_W_Two_TS.xes.gz", config,
+                   "../event_logs/{}/BPI_Challenge_2017_W_Two_TS_estimated.xes.gz".format(folder))
 
     # Call centre
     config = Configuration(
@@ -230,9 +138,63 @@ def main():
         resource_availability_type=ResourceAvailabilityType.SIMPLE,
         heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
         outlier_statistic=OutlierStatistic.MEDIAN,
-        outlier_threshold=2.0
+        outlier_threshold=outlier_threshold
     )
-    run_estimation("../event_logs/callcentre.xes.gz", config, ".xes.gz")
+    run_estimation("../event_logs/callcentre.xes.gz", config,
+                   "../event_logs/{}/callcentre_estimated.xes.gz".format(folder))
+
+    # Consulta Data Mining 2016 - 2018
+    config = Configuration(
+        log_ids=DEFAULT_XES_IDS,
+        re_estimation_method=ReEstimationMethod.MEDIAN,
+        concurrency_oracle_type=ConcurrencyOracleType.HEURISTICS,
+        resource_availability_type=ResourceAvailabilityType.SIMPLE,
+        instant_activities={"Notificacion estudiante cancelacion soli", "Traer informacion estudiante - banner"},
+        heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
+        outlier_statistic=OutlierStatistic.MEDIAN,
+        outlier_threshold=outlier_threshold
+    )
+    run_estimation("../event_logs/ConsultaDataMining201618.xes.gz", config,
+                   "../event_logs/{}/ConsultaDataMining201618_estimated.xes.gz".format(folder))
+
+    # Insurance
+    config = Configuration(
+        log_ids=DEFAULT_XES_IDS,
+        re_estimation_method=ReEstimationMethod.MEDIAN,
+        concurrency_oracle_type=ConcurrencyOracleType.HEURISTICS,
+        resource_availability_type=ResourceAvailabilityType.SIMPLE,
+        heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
+        outlier_statistic=OutlierStatistic.MEDIAN,
+        outlier_threshold=outlier_threshold
+    )
+    run_estimation("../event_logs/insurance.xes.gz", config,
+                   "../event_logs/{}/insurance_estimated.xes.gz".format(folder))
+
+    # POC Process Mining
+    config = Configuration(
+        log_ids=DEFAULT_XES_IDS,
+        re_estimation_method=ReEstimationMethod.MEDIAN,
+        concurrency_oracle_type=ConcurrencyOracleType.HEURISTICS,
+        resource_availability_type=ResourceAvailabilityType.SIMPLE,
+        heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
+        outlier_statistic=OutlierStatistic.MEDIAN,
+        outlier_threshold=outlier_threshold
+    )
+    run_estimation("../event_logs/poc_processmining.xes.gz", config,
+                   "../event_logs/{}/poc_processmining_estimated.xes.gz".format(folder))
+
+    # Production
+    config = Configuration(
+        log_ids=DEFAULT_XES_IDS,
+        re_estimation_method=ReEstimationMethod.MEDIAN,
+        concurrency_oracle_type=ConcurrencyOracleType.HEURISTICS,
+        resource_availability_type=ResourceAvailabilityType.SIMPLE,
+        heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9),
+        outlier_statistic=OutlierStatistic.MEDIAN,
+        outlier_threshold=outlier_threshold
+    )
+    run_estimation("../event_logs/Production.xes.gz", config,
+                   "../event_logs/{}/Production_estimated.xes.gz".format(folder))
 
 
 if __name__ == '__main__':
