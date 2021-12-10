62a00364d586229dc0179e26dae13dde5e38cafb
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2021-12-10 15:34:50 +0200

    Preprocess confidential to transform the message events duration into waiting time

--- processing/preprocessing.py
+++ processing/preprocessing.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import pm4py
 from pm4py.algo.filtering.log.attributes import attributes_filter
 
@@ -41,6 +43,20 @@ def preprocess_pharmacy():
     )
     # Export ensuring the timestamps has only milliseconds, not microseconds
     export_event_log_only_millis(event_log, "../event_logs/cvs_pharmacy_preproc.xes")
+
+
+def preprocess_confidential():
+    # Read event log
+    event_log = pd.read_csv("../event_logs/confidential_old.csv.gz")
+    # Make instant the 'message' events
+    instant_events = ["M1", "M2", "M3", "M4", "M5", "M6"]
+    event_log['start_time'] = np.where(
+        event_log['Activity'].isin(instant_events),
+        event_log['end_time'],
+        event_log['start_time']
+    )
+    # Export ensuring the timestamps has only milliseconds, not microseconds
+    event_log.to_csv("../event_logs/confidential.csv.gz", encoding='utf-8', index=False, compression='gzip')
 
 
 if __name__ == '__main__':
