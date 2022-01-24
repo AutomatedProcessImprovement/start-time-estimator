13f5cb5c31c2fed537e11c239a886e9da0b549c8
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2022-01-24 19:09:31 +0200

    Upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='estimate_start_times',
-    version='0.3.0',
+    version='0.4.0',
     packages=find_packages(where='estimate_start_times'),
     package_dir={"": "estimate_start_times"},
     include_package_data=True,
