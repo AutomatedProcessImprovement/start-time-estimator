84edec76a2e0067a0df1a81c141a0256b775fe33
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2022-02-01 19:38:54 +0200

    Update metrics adding different EMDs and improving the cycle time EMD binning

--- processing/simulation_metrics.py
+++ processing/simulation_metrics.py
@@ -1,3 +1,5 @@
+import datetime
+import enum
 import math
 import os
 from collections import Counter
@@ -19,7 +21,7 @@ logs = [
     "Production"
 ]
 raw_path = "../event_logs/{}.csv.gz"
-simulated_path = "../event_logs/simulated/{}"
+simulated_logs_path = "../event_logs/simulated/{}/{}/{}_simulated_1.csv"
 simulated_log_IDs = EventLogIDs(
     case="caseid",
     activity="task",
@@ -29,9 +31,16 @@ simulated_log_IDs = EventLogIDs(
 )
 
 
+class _EmdType(enum.Enum):
+    BOTH = 0
+    START = 1
+    END = 2
+
+
 def measure_simulation():
     techniques = ["raw", "heur_median", "heur_median_2", "heur_median_5", "heur_mode", "heur_mode_2", "heur_mode_5"]
-    print("dataset,simulated_from,absolute_hour_emd,trace_duration_emd,#traces_original,#traces_simulated")
+    print("dataset,simulated_from,start_time_hour_emd,end_time_hour_emd,absolute_hour_emd,"
+          "start_time_day_emd,end_time_day_emd,absolute_day_emd,trace_duration_emd")
     for log_name in logs:
         raw_event_log = read_and_preprocess_log(raw_path.format(log_name), DEFAULT_CSV_IDS)
         for technique in techniques:
@@ -40,44 +49,79 @@ def measure_simulation():
 
 def calculate_simulation_stats(log_name: str, method: str, raw_event_log: pd.DataFrame):
     # Measure stats for estimated log
-    simulated_logs_path = simulated_path.format(log_name + "/" + method + "/sim_data/")
-    simulated_event_log = None
-    if os.path.exists(simulated_logs_path):
-        for file_name in os.listdir(simulated_logs_path):
-            tmp_event_log = read_and_preprocess_log(simulated_logs_path + file_name, simulated_log_IDs)
-            tmp_event_log = tmp_event_log[~tmp_event_log[simulated_log_IDs.activity].isin(['Start', 'End'])]
-            if simulated_event_log is None:
-                simulated_event_log = tmp_event_log
-            else:
-                max_case_id = simulated_event_log[simulated_log_IDs.case].max()
-                tmp_event_log[simulated_log_IDs.case] = tmp_event_log[simulated_log_IDs.case] + max_case_id + 1
-                simulated_event_log = simulated_event_log.append(tmp_event_log)
-        hour_emd = absolute_hour_emd(raw_event_log, DEFAULT_CSV_IDS, simulated_event_log, simulated_log_IDs)
-        duration_emd = trace_duration_emd(raw_event_log, DEFAULT_CSV_IDS, simulated_event_log, simulated_log_IDs, 100)
-        print("{},{},{},{},{},{}".format(
+    simulated_log_path = simulated_logs_path.format(log_name, method, log_name)
+    if os.path.exists(simulated_log_path):
+        simulated_event_log = read_and_preprocess_log(simulated_log_path, simulated_log_IDs)
+        simulated_event_log = simulated_event_log[~simulated_event_log[simulated_log_IDs.activity].isin(['Start', 'End'])]
+        # Calculate cycle time bin size w.r.t. the original log
+        bin_size = max(
+            [events[DEFAULT_CSV_IDS.end_time].max() - events[DEFAULT_CSV_IDS.start_time].min()
+             for case, events in raw_event_log.groupby([DEFAULT_CSV_IDS.case])]
+        ) / 100
+        print("{},{},{},{},{},{},{},{},{}".format(
             log_name,
             method,
-            hour_emd,
-            duration_emd,
-            len(raw_event_log[DEFAULT_CSV_IDS.case].unique()),
-            len(simulated_event_log[simulated_log_IDs.case].unique())
+            absolute_hour_emd(raw_event_log, DEFAULT_CSV_IDS, simulated_event_log, simulated_log_IDs, _EmdType.START, discretize_to_hour),
+            absolute_hour_emd(raw_event_log, DEFAULT_CSV_IDS, simulated_event_log, simulated_log_IDs, _EmdType.END, discretize_to_hour),
+            absolute_hour_emd(raw_event_log, DEFAULT_CSV_IDS, simulated_event_log, simulated_log_IDs, _EmdType.BOTH, discretize_to_hour),
+            absolute_hour_emd(raw_event_log, DEFAULT_CSV_IDS, simulated_event_log, simulated_log_IDs, _EmdType.START, discretize_to_day),
+            absolute_hour_emd(raw_event_log, DEFAULT_CSV_IDS, simulated_event_log, simulated_log_IDs, _EmdType.END, discretize_to_day),
+            absolute_hour_emd(raw_event_log, DEFAULT_CSV_IDS, simulated_event_log, simulated_log_IDs, _EmdType.BOTH, discretize_to_day),
+            trace_duration_emd(raw_event_log, DEFAULT_CSV_IDS, simulated_event_log, simulated_log_IDs, bin_size)
         ))
 
 
-def absolute_hour_emd(event_log_1: pd.DataFrame, log_1_ids: EventLogIDs, event_log_2: pd.DataFrame, log_2_ids: EventLogIDs) -> float:
-    # Get the first date of the log
-    interval_start = min(event_log_1[log_1_ids.start_time].min(), event_log_2[log_2_ids.start_time].min())
-    interval_end = max(event_log_1[log_1_ids.end_time].max(), event_log_2[log_2_ids.end_time].max())
+def discretize_to_hour(seconds: int):
+    return math.floor(seconds / 3600)
+
+
+def discretize_to_day(seconds: int):
+    return math.floor(seconds / 3600 / 24)
+
+
+def absolute_hour_emd(
+        event_log_1: pd.DataFrame,
+        log_1_ids: EventLogIDs,
+        event_log_2: pd.DataFrame,
+        log_2_ids: EventLogIDs,
+        emd_type: _EmdType = _EmdType.BOTH,
+        discretize=discretize_to_hour  # function to discretize a total amount of seconds
+) -> float:
+    # Get the first and last dates of the log
+    if emd_type == _EmdType.BOTH:
+        interval_start = min(event_log_1[log_1_ids.start_time].min(), event_log_2[log_2_ids.start_time].min())
+        interval_end = max(event_log_1[log_1_ids.end_time].max(), event_log_2[log_2_ids.end_time].max())
+    elif emd_type == _EmdType.START:
+        interval_start = min(event_log_1[log_1_ids.start_time].min(), event_log_2[log_2_ids.start_time].min())
+        interval_end = max(event_log_1[log_1_ids.start_time].max(), event_log_2[log_2_ids.start_time].max())
+    else:
+        interval_start = min(event_log_1[log_1_ids.end_time].min(), event_log_2[log_2_ids.end_time].min())
+        interval_end = max(event_log_1[log_1_ids.end_time].max(), event_log_2[log_2_ids.end_time].max())
     # Create empty histograms
-    histogram_1 = [0] * math.floor(((interval_end - interval_start).total_seconds() / 3600) + 1)
+    histogram_1 = [0] * (discretize((interval_end - interval_start).total_seconds()) + 1)
     histogram_2 = histogram_1.copy()
     # Increase, for each time, the value in its corresponding bin
-    absolute_hours = [math.floor(difference.total_seconds() / 3600) for difference in (event_log_1[log_1_ids.start_time] - interval_start)]
-    absolute_hours += [math.floor(difference.total_seconds() / 3600) for difference in (event_log_1[log_1_ids.end_time] - interval_start)]
+    absolute_hours = []
+    if emd_type != _EmdType.END:
+        absolute_hours += [
+            discretize(difference.total_seconds()) for difference in (event_log_1[log_1_ids.start_time] - interval_start)
+        ]
+    if emd_type != _EmdType.START:
+        absolute_hours += [
+            discretize(difference.total_seconds()) for difference in (event_log_1[log_1_ids.end_time] - interval_start)
+        ]
     for absolute_hour, frequency in Counter(absolute_hours).items():
         histogram_1[absolute_hour] = frequency
-    absolute_hours = [math.floor(difference.total_seconds() / 3600) for difference in (event_log_2[log_2_ids.start_time] - interval_start)]
-    absolute_hours += [math.floor(difference.total_seconds() / 3600) for difference in (event_log_2[log_2_ids.end_time] - interval_start)]
+    # Increase, for each time, the value in its corresponding bin
+    absolute_hours = []
+    if emd_type != _EmdType.END:
+        absolute_hours += [
+            discretize(difference.total_seconds()) for difference in (event_log_2[log_2_ids.start_time] - interval_start)
+        ]
+    if emd_type != _EmdType.START:
+        absolute_hours += [
+            discretize(difference.total_seconds()) for difference in (event_log_2[log_2_ids.end_time] - interval_start)
+        ]
     for absolute_hour, frequency in Counter(absolute_hours).items():
         histogram_2[absolute_hour] = frequency
     # Return EMD metric
@@ -89,7 +133,7 @@ def trace_duration_emd(
         log_1_ids: EventLogIDs,
         event_log_2: pd.DataFrame,
         log_2_ids: EventLogIDs,
-        num_bins: int
+        bin_size: datetime.timedelta
 ) -> float:
     # Get trace durations for both logs
     trace_durations_1 = []
@@ -99,9 +143,9 @@ def trace_duration_emd(
     for case, events in event_log_2.groupby([log_2_ids.case]):
         trace_durations_2 += [events[log_2_ids.end_time].max() - events[log_2_ids.start_time].min()]
     # Get size of the bin
-    min_duration = max(trace_durations_1 + trace_durations_2)
-    max_duration = min(trace_durations_1 + trace_durations_2)
-    bin_size = (max_duration - min_duration) / (num_bins - 1)
+    min_duration = min(trace_durations_1 + trace_durations_2)
+    max_duration = max(trace_durations_1 + trace_durations_2)
+    num_bins = math.floor((max_duration - min_duration) / bin_size) + 1
     # Create empty histograms
     histogram_1 = [0] * num_bins
     histogram_2 = histogram_1.copy()
