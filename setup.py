cf4b30622fe0c7745d74564b6d5f2547b4a4a3a2
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-02-10 16:44:01 +0200

    Fix bug when estimating enabled times without start_time available

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='estimate_start_times',
-    version='1.7.0',
+    version='1.7.1',
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
